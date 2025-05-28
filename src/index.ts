2fb8a965822092ad6aa0615d2d011f13402e8b0f
jxwatson251 <jxwatson251@gmail.com>
2025-05-28 08:16:17 -0400

    feat: implement index file in src folder

--- src/index.ts
+++ src/index.ts
@@ -12,4 +12,4 @@ app.use(express.json());
 app.use('/api', authRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
