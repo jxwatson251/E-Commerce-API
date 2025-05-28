e685dc500b237a2b2ec46b4d51b76935823050e5
jxwatson251 <jxwatson251@gmail.com>
2025-05-28 09:13:19 -0400

    feat: implement Product file in models folder

---
+++ src/models/Product.ts
@@ -0,0 +1,26 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IProduct extends Document {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+  img: string;
+  imageUrl?: string;
+  description?: string;
+  category?: string;
+}
+
+const ProductSchema: Schema<IProduct> = new Schema({
+  id: {type: String, required: true},
+  name: {type: String, required: true},
+  price: {type: Number, required: true},
+  quantity: {type: Number, required: true},
+  img: {type: String},
+  imageUrl: {type: String},
+  description: {type: String},
+  category: {type: String, required: true},
+});
+
+const Product = mongoose.model<IProduct>('Product', ProductSchema);
+export default Product;
