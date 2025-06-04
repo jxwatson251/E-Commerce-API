de8a65cddbe93e9a4fe0b7c74c681294b29bbad4
jxwatson251 <jxwatson251@gmail.com>
2025-06-04 09:38:03 -0400

    fix: correct code on authenRoutes file in routes foder

--- src/routes/authenRoutes.ts
+++ src/routes/authenRoutes.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
-import { register, login, getProfile, updateProfile } from '../controllers/authenController';
+import { register, login, getProfile, updateProfile } from '../controllers/authencontroller';
 import { authenMiddleware } from '../middleware/authenMiddleware';
 import { validateBody } from '../middleware/validate';
 import { registerSchema, loginSchema } from '../utils/validator';
 
 const router = Router();
 
-router.post('/register', authenMiddleware, register);
-router.post('/login', authenMiddleware, login);
+router.post('/register', validateBody(registerSchema), register);
+router.post('/login', validateBody(loginSchema), login);
 
 router.get('/profile', authenMiddleware, getProfile);
 router.put('/profile', authenMiddleware, updateProfile);
