2873898eb0d607930a4516d4bf383f4e75b4d166
jxwatson251 <jxwatson251@gmail.com>
2025-07-01 11:36:12 -0400

    fix: correct mongodb connect code on .env

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const connectDB = async () => {
+    // console.log(process.env.MONGO_URI)
     try {
         await mongoose.connect (process.env.MONGO_URI!);
         console.log('MongoDB connected');
